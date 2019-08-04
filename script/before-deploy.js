3f1b5eb2c0480bbb24ebe36ca286646b1d95693c
toyobayashi <lifenglin314@outlook.com>
2019-08-04 17:55:35 +0800

    travis npm

--- script/before-deploy.js
+++ script/before-deploy.js
@@ -9,12 +9,12 @@ const nodeVersion = process.versions.node.replace(/\./g, '_')
 const arch = process.arch
 const platform = process.platform
 
-const packageVersion = require(getPath('package.json')).version
+// const packageVersion = require(getPath('package.json')).version
 
 if (fs.existsSync(getPath('dist/lame.node'))) {
   fs.mkdirsSync(getPath('release'))
   fs.copySync(
     getPath('dist/lame.node'),
-    getPath(`release/lame-v${packageVersion}-${platform}-${arch}-${nodeVersion}.node`)
+    getPath(`release/lame-${process.env.TRAVIS_TAG}-${platform}-${arch}-${nodeVersion}.node`)
   )
 }
