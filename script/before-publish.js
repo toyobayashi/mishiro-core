5ede38f72c7eebc024beacd4ef2e12caaa43aa51
toyobayashi <lifenglin314@outlook.com>
2020-07-17 15:48:43 +0800

    ci publish

--- script/before-publish.js
+++ script/before-publish.js
@@ -1,11 +1,7 @@
 const path = require('path')
 const fs = require('fs-extra')
 
-if (process.env.NPMTOKEN) {
-  fs.writeFileSync(path.join(__dirname, '../.npmrc'), `//registry.npmjs.org/:_authToken=${process.env.NPMTOKEN}
+fs.writeFileSync(path.join(__dirname, '../.npmrc'), `//registry.npmjs.org/:_authToken=\${NPMTOKEN}
 registry=https://registry.npmjs.org/
 always-auth=true
-`)
-} else {
-  process.exit(1)
-}
+`, 'utf8')
