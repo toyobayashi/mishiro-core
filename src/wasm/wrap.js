f54366ebc242f2b7cc01bd38dbd87321a4f6de93
toyobayashi <lifenglin314@outlook.com>
2021-05-30 21:09:47 +0800

    return boolean

--- src/wasm/wrap.js
+++ src/wasm/wrap.js
@@ -1,9 +1,9 @@
 /* eslint-disable no-undef */
 
 exports.transcodeAac = function (input, output, bitRate = 160000) {
-  Module.transcodeAac(input, output, bitRate)
+  return Module.transcodeAac(input, output, bitRate) === 0
 }
 
 /* exports.transcoding = function (input, output, bitRate = 128000) {
-  Module.transcoding(input, output, bitRate)
+  return Module.transcoding(input, output, bitRate) === 0
 } */
