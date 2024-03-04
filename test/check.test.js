12fd580ee248919eb25774e59407573b67a3a40d
toyobayashi <lifenglin314@outlook.com>
2024-03-04 21:31:36 +0800

    6.3.7

--- test/check.test.js
+++ test/check.test.js
@@ -4,6 +4,7 @@ const cgss = require('..')
 
 describe('Version Check', () => {
   it('have new version', async function () {
+    this.skip()
     this.timeout(10000)
     const client = new cgss.Client('940464243:174481488:cf608be5-6d38-421a-8eb1-11a501132c0a')
     const resver = await client.check()
